236bc48117eebba2f42b2ca7ba1a75bc25d705d3
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-07-24 12:53:17 +0200

    updated kieker based monitoring. Now available for all nodes.

--- account-service/src/main/java/org/mybatis/jpetstore/rest/InsertAccountServlet.java
+++ account-service/src/main/java/org/mybatis/jpetstore/rest/InsertAccountServlet.java
@@ -1,3 +1,19 @@
+/**
+ *    Copyright (C) 2010-2017 the original author or authors.
+ *                  2017 iObserve Project (https://www.iobserve-devops.net)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.mybatis.jpetstore.rest;
 
 import java.io.IOException;
@@ -50,7 +66,7 @@ public class InsertAccountServlet extends AbstractAccountServlet {
         final Account account = mapper.readValue(request.getReader(), Account.class);
 
         if (account != null) {
-        	LOG.error("account " + account.toString());
+            InsertAccountServlet.LOG.error("account " + account.toString());
             this.service.insertAccount(account);
         } else {
             InsertAccountServlet.LOG.error("insert-account NO ACCOUNT to insert");
--- order-service/src/main/java/org/mybatis/jpetstore/rest/InsertOrderServlet.java
+++ order-service/src/main/java/org/mybatis/jpetstore/rest/InsertOrderServlet.java
@@ -1,3 +1,19 @@
+/**
+ *    Copyright (C) 2010-2017 the original author or authors.
+ *                  2017 iObserve Project (https://www.iobserve-devops.net)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.mybatis.jpetstore.rest;
 
 import java.io.IOException;
@@ -50,7 +66,7 @@ public class InsertOrderServlet extends AbstractOrderServlet {
         final Order order = mapper.readValue(request.getReader(), Order.class);
 
         if (order != null) {
-        	LOG.error("order " + order.toString());
+            InsertOrderServlet.LOG.error("order " + order.toString());
             this.service.insertOrder(order);
         } else {
             InsertOrderServlet.LOG.error("insert-order NO ORDER to insert");
