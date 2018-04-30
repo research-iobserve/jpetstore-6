58ea4ee52a20dc572c9b3c83c528b7148503d87a
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-04-30 09:55:10 +0200

    added support for dynamic logger host.

--- frontend-service/entrypoint.sh
+++ frontend-service/entrypoint.sh
@@ -8,6 +8,14 @@ trap terminate SIGINT SIGTERM EXIT
 
 echo $HOSTNAME
 
+if [ "$LOGGER" == "" ] ; then
+	LOGGER="172.17.0.1"
+fi
+
+echo "Logger $LOGGER"
+
+echo "$LOGGER logger" >> /etc/hosts
+
 cat /server.xml.template | sed "s/%HOSTNAME%/$HOSTNAME/g" > $CATALINA_HOME/conf/server.xml
 
 catalina.sh start
